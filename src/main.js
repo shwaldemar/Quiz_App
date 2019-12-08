d61bc9d5f9fd2eb71484305a81f33988d2a4f3e3
Simon <shwaldemar@gmail.com>
2019-12-08 16:28:28 +0000

    added vue bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+
+import App from './App.vue';
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
