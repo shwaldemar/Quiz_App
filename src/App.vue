4b5b5e19d14ced581a6094bd90e4a9227d7c9e7b
Simon <shwaldemar@gmail.com>
2019-12-08 16:54:58 +0000

    added boilerplate bootstrap nav for header and jumbotron for questionbox to be tweaked to fit quiz app format

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-  <h1>Hi Simon</h1>
+  
   <Header/>
   <QuestionBox/>
   </div>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="">
-    <h1>Header</h1>
+    <b-nav tabs>
+      <b-nav-item disabled><b>Quiz App</b></b-nav-item>
+      <b-nav-item disabled>Counter:  4/10</b-nav-item>
+    </b-nav>
+
   </div>
 </template>
 
