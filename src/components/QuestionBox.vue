8b11473cd359bed28a92fa9069da1bc777e04dc8
Simon <shwaldemar@gmail.com>
2019-12-08 21:00:46 +0000

    added shuffle function to mixup order of answers in questionBox

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -9,11 +9,17 @@
       <hr class="my-4">
 
       <b-list-group>
-        <b-list-group-item v-for="(answer, index) in answers" :key="index">{{answer}}</b-list-group-item>
+        <b-list-group-item
+          v-for="(answer, index) in answers" :key="index"
+          @click.prevent="selectAnswer(index)"
+          :class="[selectedIndex === index ? 'selected' : '']"
+          >
+          {{answer}}
+        </b-list-group-item>
       </b-list-group>
       <!-- <p v-for="(answer, index) in answers" :key="index">{{answer}}</p> -->
 
-      <b-button variant="primary" href="#">Submit Answer</b-button>
+      <b-button  variant="primary" href="#">Submit Answer</b-button>
       <b-button @click="next" variant="success" href="#">Next Question</b-button>
     </b-jumbotron>
     </div>
@@ -21,10 +27,37 @@
 </template>
 
 <script>
+import _ from 'lodash'
+
 export default {
+  data(){
+    return {
+      selectedIndex: null,
+      shuffledAnswers: []
+    }
+  },
   props: {
     currentQuestion: Object,
     next: Function,
+  },
+  methods: {
+    selectAnswer: function(index){
+      this.selectedIndex = index
+    },
+    shuffleAnswers: function(){
+      let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+      this.shuffledAnswers = _.shuffle(answers)
+      return this.shuffledAnswers
+    }
+  },
+  watch: {
+    currentQuestion: {
+      immediate: true,
+      handler(){
+        this.selectedIndex = null
+        this.shuffleAnswers()
+      }
+    }
   },
   computed: {
     answers(){
@@ -36,4 +69,23 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.list-group {
+  margin-bottom: 15px
+}
+.btn {
+  margin: 5px 5px;
+}
+.list-group-item:hover {
+  cursor: pointer;
+  background-color: lightgrey;
+}
+.selected {
+  background-color: lightblue;
+}
+.correct {
+  background-color: lightgreen;
+}
+.incorrect {
+  background-color: red;
+}
 </style>
